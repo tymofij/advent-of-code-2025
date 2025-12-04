f0b15c5512504b416a9a417b8bad7445c2076199
Tim Babych <tymofiib@zillowgroup.com>
2025-12-04 03:08:30 -0500

    no realloc

--- 04/rolls_b.py
+++ 04/rolls_b.py
@@ -25,27 +25,28 @@ max_row = len(input)-1
 max_col = len(input[0])-1
 total = 0
 
-new = copy.deepcopy(input)
 has_change = True
 
 while has_change:
     has_change = False
-    new = copy.deepcopy(input)
     for row in range(max_row+1):
         for col in range(max_col+1):
-            new[row][col] = input[row][col]
             n = 0
             if input[row][col] != '@':
                 continue
             for (a, b) in shifts:
                 r, c = row + a, col + b
                 if (r>=0 and c>=0 and r<=max_row and c<=max_col
-                    and input[r][c] == "@"):
+                    and (input[r][c] == "@" or input[r][c] =="x") ):
                     n += 1
             if n < 4:
                 total += 1
-                new[row][col] = '.'
+                input[row][col] = 'x'
                 has_change = True
-    input = copy.deepcopy(new)
+    # cleanup
+    for row in range(max_row+1):
+        for col in range(max_col+1):
+            if input[row][col] == 'x':
+                input[row][col] = '.'
 
 print(total)
